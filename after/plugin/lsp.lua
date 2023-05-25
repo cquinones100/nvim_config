7dbe1ee54d91f36aaf5a7959013280f522a88325
Carlos Quinones <cquinones100@gmail.com>
2023-05-25 17:34:18 -0400

    add some lsp config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,7 +10,20 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 -- (Optional) Configure lua language server for neovim
+
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    ['<Tab>'] = cmp_action.tab_complete(),
+    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }
+})
+
 lsp.setup()
+
 
