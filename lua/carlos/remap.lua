f298566a9869b85c2dbdadde8b25a01f6619a455
Carlos Quinones <cquinones100@gmail.com>
2023-05-25 11:04:33 -0400

    mapping fixes

--- lua/carlos/remap.lua
+++ lua/carlos/remap.lua
@@ -1,4 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<C-w>", vim.cmd.q)
+vim.keymap.set("n", "<C-W>", vim.cmd.q)
 vim.keymap.set("n", "<C-s>", vim.cmd.w)
+vim.wo.relativenumber = true
+vim.wo.number = true
