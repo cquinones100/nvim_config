15c39e6385dc90d2a682e9e56d0fa0cfe22eafaf
Carlos Quinones <cquinones100@gmail.com>
2023-05-30 07:38:27 -0400

    add solargraph

--- lua/carlos/init.lua
+++ lua/carlos/init.lua
@@ -14,3 +14,46 @@ vim.opt.expandtab = true
 vim.opt.termguicolors = true
 
 vim.opt.colorcolumn = "80"
+
+vim.opt.statusline = "%n"
+
+local nvim_lsp = require('lspconfig')
+local handlers = {
+  ["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = true
+  }
+  )
+}
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+nvim_lsp.solargraph.setup {
+  cmd = {
+    "bundle",
+    "exec",
+    "solargraph",
+    "stdio"
+  },
+  filetypes = {
+    "ruby"
+  },
+  flags = {
+    debounce_text_changes = 150
+  },
+  on_attach = on_attach,
+  root_dir = nvim_lsp.util.root_pattern("Gemfile", ".git", "."),
+  capabilities = capabilities,
+  handlers = handlers,
+  settings = {
+    solargraph = {
+      autoformat = true,
+      completion = true,
+      diagnostic = true,
+      folding = true,
+      references = true,
+      rename = true,
+      symbols = true
+    }
+  }
+}
