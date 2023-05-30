bc4891e9841a37537e439d7c5886d24a74776f94
Carlos Quinones <cquinones100@gmail.com>
2023-05-30 07:48:16 -0400

    add full line yank

--- lua/carlos/remap.lua
+++ lua/carlos/remap.lua
@@ -9,3 +9,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- keep cursor in the middle when jumping up or down
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "Y", "yy")
+
