240364692d7de712e7d31e7f3194ed89691a569f
Carlos Quinones <cquinones100@gmail.com>
2023-05-30 07:56:10 -0400

    toggle terminal

--- lua/carlos/remap.lua
+++ lua/carlos/remap.lua
@@ -10,5 +10,38 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-vim.keymap.set("n", "Y", "yy")
+-- yank whole line shift+y
+vim.keymap.set("n", "<S-y>", "yy")
+
+_G.toggleTerminal = function()
+  local buftype = vim.api.nvim_buf_get_option(0, 'buftype')
+  local terminalBufNr = vim.fn.bufnr('terminal')
+
+  if buftype == 'terminal' then
+    -- hide the terminal in a tab to avoid killing it
+
+    -- move the terminal into a new tab
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-w>T', true, true, true), 'n', true)
+
+    -- go to the last visited tab
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('g<tab>', true, true, true), 'n', true)
+  else
+    _G.has_toggled_terminal = true
+
+    -- create a split pane below
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-w>s', true, true, true), 'n', true)
+
+    -- move to the new pane
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-w>j', true, true, true), 'n', true)
+
+    -- open the terminal
+    vim.api.nvim_feedkeys(':te', 'n', false)
+
+    -- carriage return the previous command
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, true, true), 'n', true)
+  end
+end
+
+-- toggle terminal
+vim.keymap.set("n", "<C-t>", ":lua toggleTerminal()<CR>")
 
