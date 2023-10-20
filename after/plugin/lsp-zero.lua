565272b2bd54bdb9bea4d90bdfa7ea0f41b3d311
Carlos Quinones <cquinones100@gmail.com>
2023-10-19 20:53:15 -0400

    use lsp go to definition on keymap

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -90,6 +90,7 @@ lsp_zero.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "<C-]>", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "gh", function() vim.diagnostic.open_float({scope="line"}) end)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
