a8757dbfa023ccf33e4c69ee52e68843290e5364
Carlos Quinones <cquinones100@gmail.com>
2023-10-30 16:08:29 -0400

    Add GitLogCurrentFile command

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -13,3 +13,13 @@ end
 vim.api.nvim_create_user_command("GitCheckoutCurrentFile", GitCheckoutCurrentFile, {
   desc = "Git Checkout Current File to Main's Version"
 })
+
+local GitLogCurrentFile = function ()
+  local file = vim.fn.expand("%:p")
+
+  vim.cmd(":Git log " .. file)
+end
+
+vim.api.nvim_create_user_command("GitLogCurrentFile", GitLogCurrentFile, {
+  desc = "Git Checkout Current File to Main's Version"
+})
