3f58b75d6acea3b692e9937da4cda3ef7b3acc29
Carlos Quinones <cquinones100@gmail.com>
2023-10-30 14:44:26 -0400

    Add GitCheckoutCurrentFile command

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,3 +1,15 @@
 vim.api.nvim_create_user_command("GitStatus", ":Git", {
   desc = "Run Git Status using Vim Fugitive"
 })
+
+local GitCheckoutCurrentFile = function ()
+  local file = vim.fn.expand("%:p")
+
+  vim.cmd(":Git checkout main " .. file)
+
+  print("checked out main " .. file)
+end
+
+vim.api.nvim_create_user_command("GitCheckoutCurrentFile", GitCheckoutCurrentFile, {
+  desc = "Git Checkout Current File to Main's Version"
+})
