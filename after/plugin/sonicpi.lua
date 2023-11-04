99ae447de5e7b7343f640bb94ecfae0a9ba94f9f
Carlos Quinones <cquinones100@gmail.com>
2023-11-04 18:49:03 -0400

    Add some commands for SonicPi

---
+++ after/plugin/sonicpi.lua
@@ -0,0 +1,29 @@
+local CurrentFile = function ()
+  local file = vim.fn.expand("%:p")
+
+  return file
+end
+
+local RunCurrentFile = function ()
+  vim.cmd("!sonic-pi-tool.py run-file " .. CurrentFile())
+end
+
+vim.api.nvim_create_user_command("SonicPiPlay", RunCurrentFile, {
+  desc = "Run current file in Sonic Pi"
+})
+
+vim.keymap.set("n", "<S-p>", RunCurrentFile)
+
+local StopCurrentFile = function ()
+  vim.cmd("!sonic-pi-tool.py stop")
+end
+
+vim.api.nvim_create_user_command("SonicPiStop", StopCurrentFile, {
+  desc = "Stop current file in Sonic Pi"
+})
+
+vim.keymap.set("n", "<S-s>", StopCurrentFile)
+
+vim.api.nvim_create_user_command("SonicPiStartServer", StopCurrentFile, {
+  desc = "Stop current file in Sonic Pi"
+})
