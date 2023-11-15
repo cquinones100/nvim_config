02bc9d7d0da59002ad96f79276eee653b8fc8b4e
Carlos Quinones <cquinones100@gmail.com>
2023-11-15 10:35:15 -0500

    Add github cli commands

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -27,3 +27,18 @@ vim.api.nvim_create_user_command("GitLogCurrentFile", GitLogCurrentFile, {
 vim.api.nvim_create_user_command("GitForcePush", "Git push origin head -f", {
   desc = "Git force push"
 })
+
+local GithubPrCreate = function ()
+  local latest_comment = vim.fn.system("git show-branch --no-name HEAD | head -n 1")
+
+  vim.fn.system("gh pr create --title \"" .. latest_comment .. "\"")
+  vim.fn.system("gh pr view --web")
+end
+
+vim.api.nvim_create_user_command("GithubPrCreate", GithubPrCreate, {
+  desc = "Create a Github PR"
+})
+
+vim.api.nvim_create_user_command("GithubPrView", "! gh pr view --web", {
+  desc = "View Github PR in browser"
+})
