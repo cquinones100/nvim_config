2c3443fbd683af59e4b6800179d5766c853ec4c9
Carlos Quinones <cquinones100@gmail.com>
2023-12-01 09:41:04 -0500

    Add remove file and copy file path commands

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,3 +1,23 @@
 local api = require "nvim-tree.api"
 
 vim.keymap.set('n', '<C-n>', api.tree.toggle)
+
+local DeleteCurrentFile = function ()
+  local file = vim.fn.expand("%:p")
+
+  vim.cmd("! rm " .. file)
+end
+
+vim.api.nvim_create_user_command("RemoveFile", DeleteCurrentFile, {
+  desc = "rm the current file"
+})
+
+function CopyRelativeFilePath()
+  local relative_file = vim.fn.expand("%")
+  vim.fn.setreg("+", relative_file)
+end
+
+vim.api.nvim_create_user_command("CopyRelativeFilePath", CopyRelativeFilePath, {
+  desc = "Copy relative path to file"
+})
+
