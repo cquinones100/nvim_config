a05e41a22d5720bcb131a1f11cd72c631def54b2
Carlos Quinones <cquinones100@gmail.com>
2024-02-05 15:15:30 -0500

    Add select all

--- lua/carlos/remap.lua
+++ lua/carlos/remap.lua
@@ -29,3 +29,8 @@ vim.keymap.set("n", "<S-c-c>", function() vim.fn.setreg("+", vim.fn.expand("%:p"
 
 -- open terminal in horizontal split
 vim.keymap.set("n", "<c-t>", function() vim.cmd("sp | resize 10 | term") end)
+
+vim.keymap.set("n", "<C-s>", vim.cmd.w)
+
+-- select all
+vim.keymap.set("n", "<C-a>", "ggVG")
