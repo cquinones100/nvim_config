cdd724948f61106bcdacabe68634841f518dc416
Carlos Quinones <cquinones100@gmail.com>
2024-02-07 11:00:05 -0500

    Switch to using a config directory in projects

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,21 +1,35 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<c-p>',
-  function()
-    builtin.find_files({
-      hidden = true,
-      file_ignore_patterns = {'.git/*', 'node_modules/*', 'vendor/*'},
-      no_ignore = true,
-    })
+local function getFindFiles()
+  local full_path = 'nvim/telescope.lua'
+  local file = io.open(full_path, "r")
+
+  local findFiles
+
+  if file then
+    local custom = require('nvim.telescope')
+
+    findFiles = custom.findFiles
+  else
+    findFiles = function()
+      builtin.find_files({
+        hidden = true,
+        no_ignore = true,
+        file_ignore_patterns = { ".git", "node_modules", "vendor", "tmp" },
+      })
+    end
   end
-)
+
+  return findFiles
+end
+
+vim.keymap.set('n', '<c-p>', getFindFiles(), {})
 
 require("telescope").load_extension("dir")
 
 vim.keymap.set('n', '<S-c-f>', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set("n", "<C-s>", vim.cmd.w)
 vim.keymap.set("n", "<S-c-f>d", "<cmd>Telescope dir live_grep<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>pd", "<cmd>Telescope dir find_files<CR>", { noremap = true, silent = true })
 vim.keymap.set({ "n", "v" }, "<S-c-p>", builtin.commands, {})
